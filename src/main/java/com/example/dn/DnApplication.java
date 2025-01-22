c5acaf6805845756646ff07ab64d1c38ff636b9a
dariyameirkan <dariya.meirkanova@icloud.com>
2025-01-22 09:10:57 +0500

    Initial commit

---
+++ src/main/java/com/example/dn/DnApplication.java
@@ -0,0 +1,13 @@
+package com.example.dn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnApplication.class, args);
+	}
+
+}
